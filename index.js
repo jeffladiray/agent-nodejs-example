b615bbbb10ef456386bb2491b3f4d02c71f37e9d
alban bertolini <albanbertolini@gmail.com>
2022-08-25 17:57:49 +0200

    adding aws cdk to try to deploy a fargat instance

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ const { createSqlDataSource } = require("@forestadmin/datasource-sql");
 
 // Create your Forest Admin agent
 createAgent({
-  // These process.env variables should be provided in the onboarding
   authSecret: process.env.FOREST_AUTH_SECRET,
   agentUrl: process.env.FOREST_AGENT_URL,
   envSecret: process.env.FOREST_ENV_SECRET,
   isProduction: process.env.NODE_ENV === "production",
-  agentUrl: process.env.FOREST_AGENT_URL,
-  forestServerUrl: process.env.FOREST_SERVER_URL,
 })
   .addDataSource(createSqlDataSource(process.env.DATABASE_URL))
-  .mountOnStandaloneServer(3000)
+  .mountOnStandaloneServer(process.env.PORT)
   .start();
