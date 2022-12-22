567aae5bdbbef040c4dece6469fc386ab73eeb5e
ShohanRahman <shohanr@forestadmin.com>
2022-12-22 11:18:36 +0100

    fix: remove agentUrl and add env file with variables

--- index.js
+++ index.js
@@ -7,10 +7,9 @@ const { createSqlDataSource } = require("@forestadmin/datasource-sql");
 // Create your Forest Admin agent
 createAgent({
   authSecret: process.env.FOREST_AUTH_SECRET,
-  agentUrl: process.env.FOREST_AGENT_URL,
   envSecret: process.env.FOREST_ENV_SECRET,
   isProduction: process.env.NODE_ENV === "production",
 })
   .addDataSource(createSqlDataSource(process.env.DATABASE_URL))
-  .mountOnStandaloneServer(process.env.PORT)
+  .mountOnStandaloneServer(process.env.APPLICATION_PORT)
   .start();
